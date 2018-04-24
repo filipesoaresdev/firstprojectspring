c0961840815bf2aefbb3a156c71374863b14951d
Filipe Soares <filipefsv@yahoo.com.br>
2018-04-24 19:09:52 -0300

    Testing how to model a class and use REST to verify. The first use of the Postman to verify the result.

--- src/main/java/com/firstproject/filipe/resources/ResourceCategory.java
+++ src/main/java/com/firstproject/filipe/resources/ResourceCategory.java
@@ -1,8 +1,14 @@
 package com.firstproject.filipe.resources;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.firstproject.filipe.domain.Category;
 
 
 //The annotation declare that this is a Rest Controller
@@ -11,8 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ResourceCategory {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String lista() {
-		return "REST está funcionando";
+	public List<Category> lista() {
+		
+		Category category1 = new Category(1, "Computing");
+		Category category2 = new Category(2, "Office");
+		
+		List<Category> categories = new ArrayList<>();
+		categories.add(category1);
+		categories.add(category2);
+		
+		
+		
+		return categories;
 		
 	}
 
