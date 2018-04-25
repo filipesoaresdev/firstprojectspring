4e8a44755c8cd10dd45906a1d65123775785f746
Filipe Soares <filipefsv@yahoo.com.br>
2018-04-24 21:44:07 -0300

    Creating instantiation operation.

--- src/main/java/com/firstproject/filipe/FirstApplication.java
+++ src/main/java/com/firstproject/filipe/FirstApplication.java
@@ -1,12 +1,32 @@
 package com.firstproject.filipe;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FirstApplication {
+import com.firstproject.filipe.domain.Category;
+import com.firstproject.filipe.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class FirstApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		
+//		Category cat1 =  new Category(null, "Computer");
+//		Category cat2 = new Category(null, "Office");
+//		
+//		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 }
