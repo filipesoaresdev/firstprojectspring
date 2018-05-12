f423243451ec4f797b99588543ceda22c305f03f
Filipe Soares <filipefsv@yahoo.com.br>
2018-05-12 17:27:24 -0300

    Customizing Category validation error

---
+++ src/main/java/com/firstproject/filipe/resources/exceptions/ValidationError.java
@@ -0,0 +1,29 @@
+package com.firstproject.filipe.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+	private List<FieldMessage> list = new ArrayList<>();
+	
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		
+		
+		
+	}
+
+
+	public List<FieldMessage> getErrors() {
+		return list;
+	}
+	
+	public void addError(String fieldName, String message) {
+		list.add(new FieldMessage(fieldName,message));
+	}
+
+	
+	
+}
